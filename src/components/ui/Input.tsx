648d84c173d5fd39dccdfd3e8916aed6d82fdede
MohamDah <mohammedfaiseldhb@gmail.com>
2025-07-03 16:44:47 +0200

    feat: add input and textarea custom components

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const inputVariants = cva(
+  "flex w-full rounded-lg px-4 py-3 bg-surface-50 border focus-visible:outline-1",
+  {
+    variants: {
+      variant: {
+        default: "border-surface-50 focus-visible:outline-surface-500",
+        error: "border-error focus-visible:outline-error",
+      }
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {
+  label?: string;
+  helperText?: string;
+  error?: string;
+}
+
+/**
+ * Input component with multiple variants
+ */
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, variant, label, error, ...props }, ref) => {
+    // If there's an error, force the variant to error
+    const inputVariant = error ? "error" : variant;
+
+    return (
+      <div className="flex flex-col w-full space-y-2">
+        {label && (
+          <label className="text-caption" htmlFor={props.id}>
+            {label}
+          </label>
+        )}
+        <input
+          className={cn(inputVariants({ variant: inputVariant }), className)}
+          ref={ref}
+          {...props}
+        />
+        {error && (
+          <p className="text-xs text-error">{error}</p>
+        )}
+      </div>
+    );
+  }
+);
+Input.displayName = "Input";
+
+export { Input, inputVariants };
