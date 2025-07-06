ef1887eb29a3bd26c49a7825665b2ab7aca9640a
MohamDah <mohammedfaiseldhb@gmail.com>
2025-07-06 16:07:43 +0200

    feat: add landing section 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./_components/HeroSection";
+import LandingSection2 from "./_components/LandingSection2";
 
 export default function Home() {
   return (
     <main className="px-36">
       <HeroSection />
+      <LandingSection2 />
     </main>
   );
 }
