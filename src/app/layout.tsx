4acdbce9ec3c25f7f9fc92c200d06efc39d8b411
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-07 02:04:16 +0200

    ci: footer initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./_components/Navbar";
+import { Footer } from "@/components/Footer";
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -27,6 +28,9 @@ export default function RootLayout({
         <Navbar />
         {children}
       </body>
+      <footer>
+        <Footer />
+      </footer>
     </html>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export function Footer() {
+    return (
+        <footer className="bg-secondary-700 py-10">
+        </footer>
+    )
+}
