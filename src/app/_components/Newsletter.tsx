06f515a626c022c50d654acf1154846ac70a3936
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-09 00:05:06 +0200

    chore: adjust newsletter's style

--- src/app/_components/Newsletter.tsx
+++ src/app/_components/Newsletter.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 
 export default function Newsletter() {
     return (
-        <div className='bg-secondary-700 py-8 flex items-center justify-center mb-24'>
-            <div className='flex flex-col items-center gap-2 justify-center'>
+        <div className='bg-secondary-700 py-16 md:py-8 flex items-center justify-center mb-24'>
+            <div className='flex flex-col px-6 text-center items-center gap-2 justify-center'>
                 <h1 className='text-heading-3 text-white'>Subscribe To Our Newsletter</h1>
-                <p className='text-surface-300/80 text-center'>Get weekly cleaning tips, exclusive guides, and special offers delivered to your <br /> inbox.</p>
+                <p className='text-surface-300/80 text-center'>Get weekly cleaning tips, exclusive guides, and special offers delivered to your inbox.</p>
                 <div className='w-full max-w-xl mt-2'>
-                    <form className='flex items-center gap-2 w-full' action="">
-                        <Input className='py-3.25' name='email' type='text' placeholder='Enter your email....' />
-                        <Button className='border-secondary-700 text-white text-lg py-3.25 hover:border-primary-700' type='submit'>Subscribe</Button>
+                    <form className='flex flex-col md:flex-row items-center gap-2 w-full' action="">
+                        <Input className='py-3.25 w-full' name='email' type='text' placeholder='Enter your email....' />
+                        <Button className='border-secondary-700 w-full text-white text-lg py-3.25 hover:border-primary-700' type='submit'>Subscribe</Button>
                     </form>
                 </div>
             </div>
