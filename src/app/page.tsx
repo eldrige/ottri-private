c663b89908b8eb436bb5a34acc45c36c3b6866cf
MohamDah <mohammedfaiseldhb@gmail.com>
2025-07-08 12:06:24 +0200

    feat: section 5 desktop version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "./_components/HeroSection";
 import LandingSection2 from "./_components/LandingSection2";
 import LandingSection3 from "./_components/LandingSection3";
+import LandingSection5 from "./_components/LandingSection5";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <LandingSection2 />
       <LandingSection3 />
+      <LandingSection5 />
     </main>
   );
 }
