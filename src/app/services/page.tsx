77913e4d17df1d12589864228588fd6bdd0bfd06
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-11 12:43:16 +0200

    ci: initial service page and add section 1

---
+++ src/app/services/page.tsx
@@ -0,0 +1,24 @@
+import { Badge } from "@/components/ui/Badge";
+import { Button } from "@/components/ui/Button";
+import React from "react";
+
+export default function ServicePage() {
+  return (
+    <section className="py-24 space-y-8">
+      <div className="text-center flex flex-col justify-center items-center space-y-4">
+        <Badge className="w-fit" variant="default" mode="outline">
+          About Ottri
+        </Badge>
+        <h2 className="text-heading-1 font-semibold">
+          Professional Cleaning Services for Every Need
+        </h2>
+        <p className="text-subtitle text-surface-500 max-w-6xl mx-auto">
+          From regular maintenance to specialized deep cleans, we offer
+          comprehensive cleaning solutions tailored to your specific
+          requirements.
+        </p>
+        <Button className="py-3 px-5 text-md">Book a cleaning now</Button>
+      </div>
+    </section>
+  );
+}
