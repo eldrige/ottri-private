fa50de681458dbd2b28ad7da7f36fedb712dceac
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-11 12:20:52 +0200

    fix: remove unnecessary import

--- src/app/_components/LandingSection6.tsx
+++ src/app/_components/LandingSection6.tsx
@@ -1,6 +1,5 @@
 import MultiImagesIcon from "@/components/icons/MultiImagesIcon";
 import VideoIcon from "@/components/icons/VideoIcon";
-import { link } from "fs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
