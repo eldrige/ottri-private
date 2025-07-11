e7c95f9d9f6634adf17a338c9cf50a6a4136b81b
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-11 11:40:57 +0200

    ci: add section 6's ui

---
+++ src/app/_components/LandingSection6.tsx
@@ -0,0 +1,118 @@
+import MultiImagesIcon from "@/components/icons/MultiImagesIcon";
+import VideoIcon from "@/components/icons/VideoIcon";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function LandingSection6() {
+  const projects = [
+    {
+      image: "/landing-section6-figure1.jpg",
+      type: "multi-images",
+    },
+    {
+      image: "/landing-section6-figure2.jpg",
+    },
+    {
+      image: "/landing-section6-figure3.jpg",
+      type: "multi-images",
+    },
+    {
+      image: "/landing-section6-figure4.jpg",
+      type: "video",
+    },
+    {
+      image: "/landing-section6-figure5.jpg",
+      type: "multi-images",
+    },
+    {
+      image: "/landing-section6-figure6.jpg",
+    },
+    {
+      image: "/landing-section6-figure7.jpg",
+      type: "video",
+    },
+    {
+      image: "/landing-section6-figure8.jpg",
+      type: "multi-images",
+    },
+    {
+      image: "/landing-section6-figure1.jpg",
+      type: "video",
+    },
+    {
+      image: "/landing-section6-figure2.jpg",
+    },
+    {
+      image: "/landing-section6-figure3.jpg",
+      type: "multi-images",
+    },
+    {
+      image: "/landing-section6-figure4.jpg",
+      type: "video",
+    },
+  ];
+
+  return (
+    <section className="py-24 space-y-8">
+      <div className="text-center space-y-4">
+        <h2 className="text-4xl font-semibold">
+          Some Of Our Cleaning Projects
+        </h2>
+        <p className="text-subtitle text-surface-500 max-w-[832px] mx-auto">
+          At OTTRI LLC, we believe in delivering more than just janitorial and
+          office supply services; we provide you with peace of mind and a clean,
+          healthy environment.
+        </p>
+      </div>
+      <div className="flex justify-end mb-6">
+        <Link
+          className="text-primary-500 hover:text-primary-600 transition-colors"
+          href="/services"
+        >
+          View all
+        </Link>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-5 auto-rows-max">
+        {projects.map((project, index) => (
+          <Link key={index} href="/services" className="block">
+            <ProjectCard
+              image={project.image}
+              type={project.type as "multi-images" | "video" | undefined}
+            />
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+}
+type ProjectCardProps = {
+  image: string;
+  type?: "multi-images" | "video";
+  height?: string;
+};
+
+function ProjectCard({ image, type }: ProjectCardProps) {
+  return (
+    <div
+      className={`relative w-full h-50 md:h-56 rounded-lg overflow-hidden shadow-md group hover:shadow-lg transition-shadow`}
+    >
+      <Image
+        src={image}
+        alt="Cleaning project"
+        fill
+        className="object-cover group-hover:scale-105 transition-transform duration-300"
+      />
+      <div className="absolute inset-0" />
+      {type && (
+        <div className="absolute z-20 top-2 right-2 rounded-full p-2 ">
+          <div className="w-5 h-5 text-gray-700">
+            {type === "multi-images" ? <MultiImagesIcon /> : <VideoIcon />}
+          </div>
+        </div>
+      )}
+      <div className="absolute inset-0 h-full w-full bg-black/30"></div>
+    </div>
+  );
+}
+///"/landing-section3-figure1.jpg"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "./_components/HeroSection";
 import LandingSection2 from "./_components/LandingSection2";
 import LandingSection3 from "./_components/LandingSection3";
+import LandingSection6 from "./_components/LandingSection6";
 import Newsletter from "./_components/Newsletter";
 
 export default function Home() {
@@ -10,10 +11,9 @@ export default function Home() {
         <HeroSection />
         <LandingSection2 />
         <LandingSection3 />
+        <LandingSection6 />
       </div>
       <Newsletter />
-
     </main>
-
   );
 }
