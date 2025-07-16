2453958331d04bebc9f8fddee46bf2e2a174ac93
MohamDah <mohammedfaiseldhb@gmail.com>
2025-07-16 17:39:41 +0200

    feat: create section 7 (map not implemented)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "./_components/HeroSection";
 import LandingSection2 from "./_components/LandingSection2";
 import LandingSection3 from "./_components/LandingSection3";
 import LandingSection5 from "./_components/LandingSection5";
+import LandingSection7 from "./_components/LandingSection7";
 import Newsletter from "./_components/Newsletter";
 
 export default function Home() {
@@ -14,6 +15,8 @@ export default function Home() {
         <LandingSection3 />
         
         <LandingSection5 />
+        
+        <LandingSection7 />
       </div>
       <Newsletter />
     </main>
