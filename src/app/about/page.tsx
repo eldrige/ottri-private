7687a5b57c574a6477be95f721ba6a22bef34e7d
MohamDah <mohammedfaiseldhb@gmail.com>
2025-07-21 17:53:30 +0200

    feat: about page section 2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AboutSection1 from './_components/AboutSection1';
+import AboutSection2 from './_components/AboutSection2';
 
 export default function AboutPage() {
   return (
     <main>
           <div className="container mx-auto px-6">
             <AboutSection1 />
+            <AboutSection2 />
           </div>
         </main>
   )
