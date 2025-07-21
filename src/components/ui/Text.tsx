5fdda0be110aa4fb1678be8c2434c77270e6dbb7
Eldrige <peldrige8@gmail.com>
2025-07-21 13:34:58 +0200

    fix: build issues

--- src/components/ui/Text.tsx
+++ src/components/ui/Text.tsx
@@ -1,26 +1,5 @@
-import { cva } from "class-variance-authority";
 import { ComponentProps } from "react";
 
-const textStyles = cva("w-full", {
-  variants: {
-    size: {
-      xs: "text-xs",
-      sm: "text-sm",
-      base: "text-base",
-      lg: "text-lg",
-      xl: "text-xl",
-      "5xl": "text-6xl"
-    },
-    weight: {
-      normal: "font-normal",
-      semibold: "font-semibold",
-    }
-  },
-  defaultVariants: {
-    size: "base",
-    weight: "normal"
-  }
-})
 
 type TextProps = ComponentProps<"span">
 
