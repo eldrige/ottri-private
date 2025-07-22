f1de29fbdf7b74452b58b096944cb56dfce37016
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-22 23:24:23 +0200

    feat: add section 2

--- src/app/how-we-work/page.tsx
+++ src/app/how-we-work/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HowWeWorkHero from "./_components/HowWeWorkHero";
 import HowWeWorkSection1 from "./_components/HowWeWorkSection1";
+import HowWeWorkSection2 from "./_components/HowWeWorkSection2";
 
 export default function HowWeWorkPage() {
   return (
@@ -8,6 +9,7 @@ export default function HowWeWorkPage() {
       <div className="container mx-auto px-6">
         <HowWeWorkHero />
         <HowWeWorkSection1 />
+        <HowWeWorkSection2 />
       </div>
     </main>
   );
