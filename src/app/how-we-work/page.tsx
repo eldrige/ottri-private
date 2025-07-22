ce4a56a33cea6cd19ae8af623df08ffbc7b2e994
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-22 17:04:10 +0200

    feat: add hero section of the How we work page

---
+++ src/app/how-we-work/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HowWeWorkHero from "./_components/HowWeWorkHero";
+
+export default function HowWeWorkPage() {
+  return (
+    <main>
+      <div className="container mx-auto px-6">
+        <HowWeWorkHero />
+      </div>
+    </main>
+  );
+}
