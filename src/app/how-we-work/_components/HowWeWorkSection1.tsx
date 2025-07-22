dda5c09e9a9ce3e612cf28c538ba5779b9e397fd
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-22 22:28:11 +0200

    fix: small responsiveness styling fixes

--- src/app/how-we-work/_components/HowWeWorkSection1.tsx
+++ src/app/how-we-work/_components/HowWeWorkSection1.tsx
@@ -6,7 +6,7 @@ import React from "react";
 export default function HowWeWorkSection1() {
   return (
     <section className="pb-10 md:pb-20">
-      <div className="flex flex-col gap-5 md:gap-20">
+      <div className="flex flex-col gap-10 md:gap-20">
         {howWeWorkData.map((elem) => (
           <HowWeWorkCard key={elem.id} {...elem} />
         ))}
@@ -32,15 +32,17 @@ function HowWeWorkCard({
         }  lg:*:flex-1 gap-8 justify-center items-center"
       `}
     >
-      <div className="flex justify-center flex-col gap-5">
+      <div className="flex justify-center flex-col gap-6">
         <div className="flex gap-4 items-center">
           <button className="bg-primary-700 text-white font-semibold px-3.75 text-center flex items-center justify-center py-2 rounded-full">
             {id}
           </button>
           <Icon className="size-8 text-primary-700" />
         </div>
-        <h2 className="text-heading-3">{title}</h2>
-        <p className="text-surface-700">{content}</p>
+        <div className="flex flex-col gap-4">
+          <h2 className="text-heading-3">{title}</h2>
+          <p className="text-surface-700">{content}</p>
+        </div>
         <ul className="text-surface-700 marker:text-primary-700 space-y-4">
           {steps.map((step, id) => (
             <li className="flex items-center" key={id}>
