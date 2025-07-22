10d94f41bb5ada41cfa79585c048276cf298f276
MohamDah <mohammedfaiseldhb@gmail.com>
2025-07-22 16:50:19 +0200

    feat: add about page section 4

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import AboutSection1 from './_components/AboutSection1';
 import AboutSection2 from './_components/AboutSection2';
 import AboutSection3 from './_components/AboutSection3';
+import AboutSection4 from './_components/AboutSection4';
 
 export default function AboutPage() {
   return (
     <main>
-      <div className="container mx-auto px-6">
+      <div className="*:container *:mx-auto *:px-6">
         <AboutSection1 />
         <AboutSection2 />
         <AboutSection3 />
+        <AboutSection4 />
       </div>
     </main>
   );
