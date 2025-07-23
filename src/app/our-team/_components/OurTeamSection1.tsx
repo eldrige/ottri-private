7d7fadec00c92b676401844603c3c21f5784126d
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-23 12:10:44 +0200

    feat: add OurTeam Page's Section 1

---
+++ src/app/our-team/_components/OurTeamSection1.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import ourTeamFigure1 from "@/assets/ourteam-figure1.jpg";
+import ourTeamFigure2 from "@/assets/ourteam-figure2.jpg";
+import ourTeamFigure3 from "@/assets/ourteam-figure3.jpg";
+import ourTeamFigure4 from "@/assets/ourteam-figure4.jpg";
+import ourTeamFigure5 from "@/assets/ourteam-figure5.jpg";
+import ourTeamFigure6 from "@/assets/ourteam-figure6.jpg";
+import Image from "next/image";
+
+export default function OurTeamSection1() {
+  return (
+    <section className="py-8 lg:py-24 flex flex-col gap-8">
+      <div className="text-center flex flex-col justify-center items-center space-y-4">
+        <h2 className="text-heading-3 md:text-heading-2 font-semibold">
+          Featured Team Members
+        </h2>
+        <p className="text-subtitle text-surface-500 text-base max-w-6xl mx-auto">
+          Our most experienced and highly-rated cleaning professionals
+        </p>
+      </div>
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+        <Image
+          className="rounded-lg aspect-[2/2.5] w-full object-cover"
+          src={ourTeamFigure1}
+          alt={` cover`}
+        />
+        <Image
+          className="rounded-lg aspect-[2/2.5] w-full object-cover"
+          src={ourTeamFigure2}
+          alt={` cover`}
+        />
+        <Image
+          className="rounded-lg aspect-[2/2.5] w-full object-cover"
+          src={ourTeamFigure3}
+          alt={` cover`}
+        />
+        <Image
+          className="rounded-lg aspect-[2/2.5] w-full object-cover"
+          src={ourTeamFigure4}
+          alt={` cover`}
+        />
+        <Image
+          className="rounded-lg aspect-[2/2.5] w-full object-cover"
+          src={ourTeamFigure5}
+          alt={` cover`}
+        />
+        <Image
+          className="rounded-lg aspect-[2/2.5] w-full object-cover"
+          src={ourTeamFigure6}
+          alt={` cover`}
+        />
+      </div>
+    </section>
+  );
+}
--- src/app/our-team/page.tsx
+++ src/app/our-team/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import OurTeamHero from "./_components/OurTeamHero";
+import OurTeamSection1 from "./_components/OurTeamSection1";
 
 export default function OurTeamPage() {
   return (
     <main>
       <div className="container mx-auto px-6">
         <OurTeamHero />
+        <OurTeamSection1 />
       </div>
     </main>
   );
