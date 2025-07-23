dec0d6bc7b9bd09c9682668e232f3f2f133010b2
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-23 11:47:04 +0200

    feat: OurTeam Page hero section

---
+++ src/app/our-team/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import OurTeamHero from "./_components/OurTeamHero";
+
+export default function OurTeamPage() {
+  return (
+    <main>
+      <div className="container mx-auto px-6">
+        <OurTeamHero />
+      </div>
+    </main>
+  );
+}
