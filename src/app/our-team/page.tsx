3c7dfd40cef6e4bbf77a48715c25eb663c14bb74
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-23 12:34:09 +0200

    feat: add OurTeam Page's Section 2

--- src/app/our-team/page.tsx
+++ src/app/our-team/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import OurTeamHero from "./_components/OurTeamHero";
 import OurTeamSection1 from "./_components/OurTeamSection1";
 import OurTeamSection3 from "./_components/OurTeamSection3";
+import OurTeamSection2 from "./_components/OurTeamSection2";
 
 export default function OurTeamPage() {
   return (
@@ -9,6 +10,9 @@ export default function OurTeamPage() {
       <div className="container mx-auto px-6">
         <OurTeamHero />
         <OurTeamSection1 />
+      </div>
+      <OurTeamSection2 />
+      <div className="container mx-auto px-6">
         <OurTeamSection3 />
       </div>
     </main>
