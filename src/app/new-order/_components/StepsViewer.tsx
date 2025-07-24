9b2f2c876f1ea5d39f4f33ba94f225fd16fce62c
MohamDah <mohammedfaiseldhb@gmail.com>
2025-07-24 19:24:46 +0200

    feat: make mobile steps progress bar dynamic

---
+++ src/app/new-order/_components/StepsViewer.tsx
@@ -0,0 +1,62 @@
+"use client";
+import CalendarIcon from "@/components/icons/CalendarIcon";
+import CardIcon from "@/components/icons/CardIcon";
+import DollarIcon from "@/components/icons/DollarIcon";
+import LocationIcon from "@/components/icons/LocationIcon";
+import MoneyIcon from "@/components/icons/MoneyIcon";
+import PawPrintIcon from "@/components/icons/PawPrintIcon";
+import RewardStars from "@/components/icons/RewardStars";
+import { cn } from "@/lib/utils";
+import { HomeIcon } from "lucide-react";
+import { useEffect, useRef } from "react";
+
+const STEPS = [
+  { text: "Service Type", Icon: RewardStars },
+  { text: "Property Details", Icon: HomeIcon },
+  { text: "Add-Ons", Icon: DollarIcon },
+  { text: "Pet Info", Icon: PawPrintIcon },
+  { text: "Access", Icon: LocationIcon },
+  { text: "Schedule", Icon: CalendarIcon },
+  { text: "Tip", Icon: MoneyIcon },
+  { text: "Payment", Icon: CardIcon },
+];
+
+
+export default function StepsViewer({currStep}: {currStep: number}) {
+  const stepsRef = useRef<(HTMLDivElement | null)[]>([]);
+
+  useEffect(() => {
+    stepsRef.current[currStep]?.scrollIntoView({
+      block: "center",
+      inline: "center",
+      behavior: "smooth"
+    });
+  }, [currStep]);
+
+  return (
+    <div className="overflow-x-hidden lg:overflow-x-auto">
+      <div className="min-w-max">
+        <div className="flex justify-between gap-6">
+          {
+            STEPS.map((step, idx) => {
+              const selected = currStep >= idx;
+              return (
+                <div key={idx} ref={el => { stepsRef.current[idx] = el; }} className="flex flex-col items-center">
+                  <span className={cn("p-2 rounded-full border transition-colors duration-300", selected ? "bg-primary-700 border-bg-primary-700 text-white" : "text-black/25 border-black/25")}>
+                    <step.Icon size={16} className="*:size-4" />
+                  </span>
+                  <p className={`mt-2 font-nunito-sans text-caption min-w-fit text-nowrap ${!selected ? "text-black/25" : ""}`}>{step.text}</p>
+                </div>
+              );
+            })
+          }
+        </div>
+        <div className="w-full mt-2 rounded-full overflow-hidden">
+          <div className="bg-secondary-700 h-2.5 transition-[width] duration-300"
+            style={{ width: `${((currStep + 1) / 8) * 100}%` }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/new-order/page.tsx
+++ src/app/new-order/page.tsx
@@ -1,31 +1,14 @@
 "use client";
-import CalendarIcon from "@/components/icons/CalendarIcon";
-import CardIcon from "@/components/icons/CardIcon";
 import CheckCircleBroken from "@/components/icons/CheckCircleBroken";
-import DollarIcon from "@/components/icons/DollarIcon";
-import LocationIcon from "@/components/icons/LocationIcon";
-import MoneyIcon from "@/components/icons/MoneyIcon";
-import PawPrintIcon from "@/components/icons/PawPrintIcon";
-import RewardStars from "@/components/icons/RewardStars";
 import { Button } from "@/components/ui/Button";
-import { cn } from "@/lib/utils";
 import { ArrowLeft, Building2, HomeIcon, Shield, Trees } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
-
-const STEPS = [
-  { text: "Service Type", Icon: RewardStars },
-  { text: "Property Details", Icon: HomeIcon },
-  { text: "Add-Ons", Icon: DollarIcon },
-  { text: "Pet Info", Icon: PawPrintIcon },
-  { text: "Access", Icon: LocationIcon },
-  { text: "Schedule", Icon: CalendarIcon },
-  { text: "Tip", Icon: MoneyIcon },
-  { text: "Payment", Icon: CardIcon },
-];
+import StepsViewer from "./_components/StepsViewer";
 
 export default function NewOrderPage() {
   const [currStep, setCurrStep] = useState(0);
+  
   return (
     <main className="container max-w-5xl mx-auto px-6 mt-2.5 py-8 text-secondary-700">
       <Link className="text-primary-700 text-subtitle flex gap-4" href="/services">
@@ -36,28 +19,7 @@ export default function NewOrderPage() {
       <div className="mt-8 space-y-8">
         <h1 className="text-heading-2.5">Book Your Cleaning Service</h1>
 
-        <div className="overflow-hidden">
-          <div className="flex justify-between">
-            {
-              STEPS.map((step, idx) => {
-                const selected = currStep >= idx;
-                return (
-                  <div key={idx} className="flex flex-col items-center ">
-                    <span className={cn("p-2 rounded-full border", selected ? "bg-primary-700 border-bg-primary-700 text-white" : "text-black/25 border-black/25")}>
-                      <step.Icon size={16} className="*:size-4" />
-                    </span>
-                    <p className={`mt-2 font-nunito-sans text-caption ${!selected ? "text-black/25" : ""}`}>{step.text}</p>
-                  </div>
-                );
-              })
-            }
-          </div>
-          <div className="w-full mt-2 rounded-full overflow-hidden">
-            <div className="bg-secondary-700 h-2.5 transition-[width] duration-300"
-              style={{ width: `${((currStep + 1) / 8) * 100}%` }}
-            />
-          </div>
-        </div>
+        <StepsViewer currStep={currStep} />
 
         <div className="mt-8 grid lg:grid-cols-3 gap-x-8 items-start">
           <div className="lg:col-span-2 lg:bg-white rounded-2xl lg:shadow-custom-light space-y-6 lg:px-8 py-8">
