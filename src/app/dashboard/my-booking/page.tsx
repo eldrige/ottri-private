53213f67a094d3d1d3065a67ffcd8f6022bfb223
Nick-Lemy <n.kayiranga@alustudent.com>
2025-07-28 02:18:32 +0200

    ci: initiate other pages of the dashboard

---
+++ src/app/dashboard/my-booking/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyBookingPage() {
+  return <div>MyBookingPage</div>;
+}
---
+++ src/app/dashboard/service-history/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ServiceHistoryPage() {
+  return <div>ServiceHistoryPage</div>;
+}
