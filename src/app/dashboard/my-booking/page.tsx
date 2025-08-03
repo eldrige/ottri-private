0fb7de342128662bc09f6a5c7cc6559aed29779e
Nick-Lemy <n.kayiranga@alustudent.com>
2025-08-03 19:15:31 +0200

    feat: add booking page

--- src/app/dashboard/my-booking/page.tsx
+++ src/app/dashboard/my-booking/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import MyBookingSection1 from "./_components/MyBookingSection1";
+import MyBookingSection2 from "./_components/MyBookingSection2";
+import MyBookingSection3 from "./_components/MyBookingSection3";
 
 export default function MyBookingPage() {
-  return <div>MyBookingPage</div>;
+  return (
+    <div className="flex flex-col mt-15 lg:mt-0 gap-6 lg:mx-6">
+      <MyBookingSection1 />
+      <MyBookingSection2 />
+      <MyBookingSection3 />
+    </div>
+  );
 }
