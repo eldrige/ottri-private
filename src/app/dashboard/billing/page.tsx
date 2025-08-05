e63232d55cfb10ea2f86de86f46ce6d341b20df3
Nick-Lemy <n.kayiranga@alustudent.com>
2025-08-06 00:47:27 +0200

    feat: add BillingPage Section 2

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BillingSection1 from "./_components/BillingSection1";
+import BillingSection2 from "./_components/BillingSection2";
 
 export default function BillingPage() {
   return (
     <div className="flex flex-col mt-15 lg:mt-0 gap-6 lg:mx-6">
       <BillingSection1 />
+      <BillingSection2 />
     </div>
   );
 }
