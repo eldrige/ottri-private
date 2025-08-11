87b02dfece85c3a6175e92bd916e3676638cf3e7
MohamDah <mohammedfaiseldhb@gmail.com>
2025-08-11 13:20:37 +0200

    fix: adjust mobile header margin and button flex properties for better layout

--- src/app/admin/_components/Header.tsx
+++ src/app/admin/_components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import logo from "@/assets/logo.png";
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
 import Link from "next/link";
 import AdminNavbar from "./AdminNavbar";
 import MenuIcon from "@/components/icons/MenuIcon";
@@ -12,22 +12,30 @@ export default function Header() {
   useEffect(() => {
     if (showMobile) {
       document.body.style.overflowY = "hidden";
-      return () => { document.body.style.overflowY = "auto"; };
+      return () => {
+        document.body.style.overflowY = "auto";
+      };
     } else {
       document.body.style.overflowY = "auto";
     }
 
-    return () => { };
+    return () => {};
   }, [showMobile]);
 
   return (
     <>
-
       {/* Desktop Header */}
       <div className="fixed top-0 left-0 py-3 pl-3 h-full">
         <header className="hidden lg:flex h-full flex-col w-60 bg-surface-50 rounded-lg p-4">
-          <Link href="/admin/" className='flex items-center gap-2.5 text-heading-5 lg:text-heading-4'>
-            <Image className='h-8 w-8 lg:h-10lg:w-10' src={logo} alt='Ottri Logo' />
+          <Link
+            href="/admin/"
+            className="flex items-center gap-2.5 text-heading-5 lg:text-heading-4"
+          >
+            <Image
+              className="h-8 w-8 lg:h-10lg:w-10"
+              src={logo}
+              alt="Ottri Logo"
+            />
             <span>Ottri</span>
           </Link>
 
@@ -40,22 +48,27 @@ export default function Header() {
             <div className="h-10 aspect-square rounded-full bg-gray-900" />
             <div>
               <p className="text-sm font-medium">Jenny Murphy</p>
-              <p className="text-xs text-secondary-700/50">jennymurphy@gmail.com</p>
+              <p className="text-xs text-secondary-700/50">
+                jennymurphy@gmail.com
+              </p>
             </div>
           </div>
         </header>
       </div>
 
-
       {/* Mobile Header */}
       <header className="sticky top-1 h-14 px-4 mx-1 mt-1 flex lg:hidden items-center justify-between text-white bg-secondary-700 rounded-2xl shadow-custom">
-        <button onClick={() => setShowMobile(!showMobile)} className="cursor-pointer p-2">
+        <button
+          onClick={() => setShowMobile(!showMobile)}
+          className="cursor-pointer p-2"
+        >
           <MenuIcon className="size-6" />
         </button>
-        <Link href="/admin" className="text-heading-5 font-semibold">Ottri</Link>
+        <Link href="/admin" className="text-heading-5 font-semibold">
+          Ottri
+        </Link>
         <div className="bg-gray-400 rounded-full h-10 aspect-square" />
       </header>
-
 
       {/* Popup Mobile Open Nav Menu */}
       <div
@@ -66,16 +79,19 @@ export default function Header() {
         onClick={() => setShowMobile(false)} // Close when clicking the overlay
       >
         <header className="min-h-14 px-4 mx-1 mt-1 flex items-center text-white bg-secondary-700 rounded-2xl shadow-custom">
-          <button onClick={(e) => {
-            e.stopPropagation(); // Prevent event from bubbling to the overlay
-            setShowMobile(!showMobile);
-          }} className="cursor-pointer p-2">
+          <button
+            onClick={(e) => {
+              e.stopPropagation(); // Prevent event from bubbling to the overlay
+              setShowMobile(!showMobile);
+            }}
+            className="cursor-pointer p-2"
+          >
             <MenuIcon className="size-6" />
           </button>
         </header>
         <div
           className={cn(
-            "flex flex-col px-4 mt-2 ml-1 mb-1 h-full w-full max-w-sm text-secondary-700 bg-white shadow-custom-strong rounded-2xl transition",
+            "flex flex-col px-4 mt-2 mx-1 mb-1 h-full max-w-sm text-secondary-700 bg-white shadow-custom-strong rounded-2xl transition",
             showMobile ? "translate-x-0" : "-translate-x-full"
           )}
           onClick={(e) => e.stopPropagation()} // Prevent clicks inside nav from closing it
@@ -85,7 +101,9 @@ export default function Header() {
             <div className="h-10 aspect-square rounded-full bg-gray-500" />
             <div>
               <p className="text-sm font-medium">Jenny Murphy</p>
-              <p className="text-xs text-secondary-700/50">jennymurphy@gmail.com</p>
+              <p className="text-xs text-secondary-700/50">
+                jennymurphy@gmail.com
+              </p>
             </div>
           </div>
         </div>
