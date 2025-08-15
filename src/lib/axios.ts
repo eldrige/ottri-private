41a36070ac3266bf3bd73a48d656eb5a26b30a0e
MohamDah <mohammedfaiseldhb@gmail.com>
2025-08-15 15:01:13 +0200

    fix: update axios baseURL to use environment variable for backend API

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,5 @@
 import Axios from "axios";
 
 export const axios = Axios.create({
-  // baseURL: "https://ottri-backend-600e2b0645fc.herokuapp.com/api/v1/"
-  baseURL: "http://172.22.11.156:3000/api/v1/"
+  baseURL: process.env.BACKEND_API
 });
