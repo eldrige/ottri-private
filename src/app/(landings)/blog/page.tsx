c65cb2d11f92494174de1b178037120b1e4f7abb
Nick-Lemy <n.kayiranga@alustudent.com>
2025-08-19 19:27:11 +0200

    feat: add hero section of blog page

---
+++ src/app/(landings)/blog/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BlogHero from "./_components/BlogHero";
+
+export default function page() {
+  return (
+    <main>
+      <div className="container mx-auto px-6">
+        <BlogHero />
+      </div>
+    </main>
+  );
+}
