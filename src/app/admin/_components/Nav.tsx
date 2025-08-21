6e5dd8f515faa8b2ab4c798b0d16343fe12b9df8
MohamDah <mohammedfaiseldhb@gmail.com>
2025-08-21 14:35:17 +0200

    feat: add end prop to NavLink for improved active link handling in AdminNavbar

--- src/app/admin/_components/Nav.tsx
+++ src/app/admin/_components/Nav.tsx
@@ -1,23 +1,45 @@
 "use client";
-import { cn } from '@/lib/utils';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import React, { ComponentProps, ReactNode } from 'react';
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { ComponentProps, ReactNode } from "react";
 
-export function Nav({ children, className }: { children: ReactNode; className?: string; }) {
+export function Nav({
+  children,
+  className
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
-    <nav className={cn("mt-8 flex flex-col gap-2.5 text-secondary-700/70 text-sm", className)}>
+    <nav
+      className={cn(
+        "mt-8 flex flex-col gap-2.5 text-secondary-700/70 text-sm",
+        className
+      )}
+    >
       <p>Navigation</p>
       {children}
     </nav>
   );
 }
 
-export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
+export function NavLink(
+  props: Omit<ComponentProps<typeof Link>, "className"> & { end?: boolean }
+) {
   const pathname = usePathname();
-  return <Link {...props}
-    className={cn(
-      "py-2 px-3 rounded-lg flex items-center gap-2",
-      pathname === props.href ? "bg-surface-75 text-secondary-700" : "hover:bg-surface-75"
-    )} />;
+  const { end, ...restProps } = props;
+  let isActive = false;
+  if (end) isActive = pathname.endsWith(props.href.toString());
+  else isActive = pathname.includes(props.href.toString());
+
+  return (
+    <Link
+      {...restProps}
+      className={cn(
+        "py-2 px-3 rounded-lg flex items-center gap-2",
+        isActive ? "bg-surface-75 text-secondary-700" : "hover:bg-surface-75"
+      )}
+    />
+  );
 }
