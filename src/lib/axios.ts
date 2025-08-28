04a6443a9d818d1d5633331dd38feddc8660a4b6
Nick-Lemy <n.kayiranga@alustudent.com>
2025-08-28 11:39:06 +0200

    fix: remove unnecessary timeout

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 // Create a custom instance
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api/v1",
-  timeout: 10000,
   headers: {
     "Content-Type": "application/json"
   }
