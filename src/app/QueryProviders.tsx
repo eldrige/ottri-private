ce542d544270154911549fc592defeb5821f14c8
MohamDah <mohammedfaiseldhb@gmail.com>
2025-09-09 11:18:10 +0200

    feat: add Providers component for React Query integration and update import in layout

--- src/app/QueryProviders.tsx
+++ src/app/QueryProviders.tsx
@@ -1,11 +1,11 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
-
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito_Sans, Poppins } from "next/font/google";
 import "./globals.css";
-import Providers from "./providers";
+import Providers from "./QueryProviders";
 
 const poppins = Poppins({
   variable: "--font-poppins",
