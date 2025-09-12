78c1b24d8dddb08355ed9187e8382d1f567a0890
Nick-Lemy <n.kayiranga@alustudent.com>
2025-09-12 23:41:59 +0200

    refactor: replace async data fetching with hooks in ProfilePage component

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,19 +1,20 @@
+"use client";
 import React from "react";
 import ProfileSection1 from "./_components/ProfileSection1";
 import ProfileSection3 from "./_components/ProfileSection3";
 import ProfileSection2 from "./_components/ProfileSection2";
-import { getBookings, getUserDetails, getUserProfile } from "../_utils/queries";
+import { useGetBookingsQuery, useGetUserProfile } from "../_services/queries";
 
-export default async function ProfilePage() {
-  const profile = await getUserProfile();
-  const user = await getUserDetails(profile.id);
-  const bookings = await getBookings();
+export default function ProfilePage() {
+  const { data: user } = useGetUserProfile();
+  const { data: bookingsData } = useGetBookingsQuery();
+  const bookings = bookingsData?.data ?? [];
   return (
     <div className="flex flex-col mt-15 lg:mt-0 gap-6 lg:mx-6">
       <ProfileSection1 />
-      <ProfileSection2 user={user} />
+      <ProfileSection2 user={user!} />
       <ProfileSection3
-        yearJoined={new Date(user.createdAt).getFullYear()}
+        yearJoined={new Date(user!.createdAt).getFullYear()}
         totalBookings={bookings.length}
         averageRating={0}
       />
