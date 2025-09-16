580d27a656655484f4a68cba785ab3f762b4df10
Nick-Lemy <n.kayiranga@alustudent.com>
2025-09-16 21:08:50 +0200

    feat: add ClientProviders component for query client and hydration management in LandingLayout so that the services get prefetched

--- src/app/(landings)/layout.tsx
+++ src/app/(landings)/layout.tsx
@@ -1,21 +1,30 @@
 import Navbar from "./_components/Navbar";
 import Footer from "@/app/(landings)/_components/Footer";
 import CookiesBanner from "./_components/CookiesBanner";
+import { dehydrate, QueryClient } from "@tanstack/react-query";
+import { getServices } from "./services/_utils/queries";
+import { ClientProviders } from "./LandingPageLayoutWrapUps";
 
-export default function LandingLayout({
+export default async function LandingLayout({
   children
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["services"],
+    queryFn: () => getServices()
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
-    <>
+    <ClientProviders dehydratedState={dehydratedState}>
       <Navbar />
       {children}
-
       <footer className="bg-secondary-700">
         <Footer />
       </footer>
       <CookiesBanner />
-    </>
+    </ClientProviders>
   );
 }
