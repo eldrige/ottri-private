2d19a34b3f54b45b526933b57741410ca2f72559
MohamDah <mohammedfaiseldhb@gmail.com>
2025-09-17 20:11:37 +0200

    feat: enhance admin middleware with validation timestamp and reduce validation frequency

--- src/app/api/bookings/route.ts
+++ src/app/api/bookings/route.ts
@@ -4,6 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   const bookings = await getBookings(req.nextUrl.searchParams);
 
-  console.log(bookings);
+  // console.log(bookings);
   return NextResponse.json(bookings.data);
 }
