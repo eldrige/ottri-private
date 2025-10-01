16394712b785c355e232aa032b777d3738bb487e
Nick-Lemy <n.kayiranga@alustudent.com>
2025-10-01 21:19:33 +0200

    fix: add width and height attributes to Image component in ServiceCard for better layout

--- src/app/(landings)/_components/LandingSection3Services.tsx
+++ src/app/(landings)/_components/LandingSection3Services.tsx
@@ -191,6 +191,8 @@ function ServiceCard({
       <Image
         className="rounded-t-lg aspect-[2/1] w-full object-cover"
         src={coverImage}
+        width={500}
+        height={500}
         alt={`${name}'s cover`}
       />
       <div className="space-y-4 px-6">
