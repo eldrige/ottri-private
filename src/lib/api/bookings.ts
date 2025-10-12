901586f7aef22959a54ffad809fc747cad9aa50b
MohamDah <mohammedfaiseldhb@gmail.com>
2025-10-12 15:55:36 +0200

    fix: Correct endpoint in getBookings function to use 'bookings' instead of 'booking'

--- src/lib/api/bookings.ts
+++ src/lib/api/bookings.ts
@@ -2,8 +2,7 @@
 import { serverRequest } from "../serverRequest";
 
 export function getBookings(searchParams: URLSearchParams) {
-  console.log(`booking?${searchParams.toString()}`);
-  return serverRequest(`booking?${searchParams.toString()}`, "GET");
+  return serverRequest(`bookings?${searchParams.toString()}`, "GET");
   // return serverFetch(`bookings?${searchParams.toString()}`, "GET")
 }
 
