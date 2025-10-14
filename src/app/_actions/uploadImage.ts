4c83ea56faca01738fd8ebe07f84dabdefebb96f
MohamDah <mohammedfaiseldhb@gmail.com>
2025-10-14 14:54:48 +0200

    feat: Refactor image upload to use clientAxios for API requests

--- src/app/_actions/uploadImage.ts
+++ src/app/_actions/uploadImage.ts
@@ -1,16 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-"use server";
-
-import { serverRequest } from "@/lib/serverRequest";
+"use client";
+import { clientAxios } from "@/lib/axios";
 
 export const uploadImage = async (image: File) => {
-  try {
-    const formData = new FormData();
-    formData.set("tag", "image");
-    formData.set("file", image);
-    const res = await serverRequest("system/upload", "POST", formData);
-    return { data: res.data as { url: string; filename: string } };
-  } catch (err: any) {
-    return { error: err.response.data.message };
-  }
+  const formData = new FormData();
+  formData.set("tag", "image");
+  formData.set("file", image);
+  const res = await clientAxios.post("system/upload", formData);
+  console.log({ res });
+  return { data: res.data as { url: string; filename: string } };
 };
