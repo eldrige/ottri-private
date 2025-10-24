a40cfe793083bea77aa0b720737e2cb800ebe550
Nick-Lemy <n.kayiranga@alustudent.com>
2025-10-24 17:31:24 +0200

    fix: update booking query to use status filter

--- src/app/dashboard/my-bookings/_components/MyBookingSection4.tsx
+++ src/app/dashboard/my-bookings/_components/MyBookingSection4.tsx
@@ -17,7 +17,11 @@ export default function MyBookingSection4() {
   );
   const [page, setPage] = useState(0);
 
-  const { data: bookings, isLoading } = useGetBookingsQuery("", 4, page);
+  const { data: bookings, isLoading } = useGetBookingsQuery(
+    statusFilter,
+    4,
+    page
+  );
 
   const pastBookings = bookings?.data || [];
 
