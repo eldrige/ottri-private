d34bcddd34a802cc99b149c920e6ea942f928cdd
Nick-Lemy <n.kayiranga@alustudent.com>
2025-10-28 23:57:51 +0200

    feat: implement article fetching and display in BlogSection1 component

--- src/app/(landings)/blog/page.tsx
+++ src/app/(landings)/blog/page.tsx
@@ -1,14 +1,13 @@
+"use client";
 import React from "react";
 import BlogHero from "./_components/BlogHero";
 import BlogSection1 from "./_components/BlogSection1";
-import { getPublishedArticles } from "./_utils/queries";
 
-export default async function page() {
-  const articles = await getPublishedArticles();
+export default function page() {
   return (
     <>
       <BlogHero />
-      <BlogSection1 articles={articles} />
+      <BlogSection1 />
     </>
   );
 }
