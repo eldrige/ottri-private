086e9b7dd34b8911c90777f29a2031b174565564
MohamDah <mohammedfaiseldhb@gmail.com>
2025-12-05 18:13:33 +0200

    fix: update document tag to 'other' in uploadDocument function

--- src/utils/uploadDocument.ts
+++ src/utils/uploadDocument.ts
@@ -3,7 +3,7 @@ import { clientAxios } from "@/lib/axios";
 
 export const uploadDocument = async (doc: File) => {
   const formData = new FormData();
-  formData.set("tag", "document");
+  formData.set("tag", "other");
   formData.set("file", doc);
   const res = await clientAxios.post("system/upload", formData);
   return { data: res.data as { url: string; filename: string } };
