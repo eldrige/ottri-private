abb677e414d400d24b510f00af38558ab056ba14
MohamDah <mohammedfaiseldhb@gmail.com>
2025-12-11 15:32:04 +0200

    feat: add BookingActions component for enhanced booking management in OverviewItem and ListItem

--- src/app/admin/(authed)/_components/OverviewItem.tsx
+++ src/app/admin/(authed)/_components/OverviewItem.tsx
@@ -1,32 +1,11 @@
-import CallIcon from "@/components/icons/CallIcon";
 import React, { useState } from "react";
 import { Booking, BookingStatusLabels } from "../../types";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/Button";
-import EditIcon from "@/components/icons/EditIcon";
-import {
-  useCancelBookingMutation,
-  useCompleteBookingMutation,
-  useStartBookingMutation
-} from "../_services/mutations";
-import TrashIcon from "@/components/icons/TrashIcon";
-import ConfirmModal from "@/components/common/ConfirmModal";
-import AssignCleaner from "../bookings/_components/AssignCleaner";
-import EditBooking from "../bookings/_components/EditBooking";
 import { format } from "date-fns";
+import BookingActions from "./BookingActions";
 import BookingDetails from "../bookings/_components/BookingDetails";
 
 export default function OverviewItem({ booking }: { booking: Booking }) {
-  const { mutateAsync: mutateCancel, isPending: isCancelling } =
-    useCancelBookingMutation();
-  const { mutateAsync: mutateStart, isPending: isStarting } =
-    useStartBookingMutation();
-  const { mutateAsync: mutateComplete, isPending: isCompleting } =
-    useCompleteBookingMutation();
-
-  const [editBooking, setEditBooking] = useState(false);
-  const [assignCleaners, setAssignCleaners] = useState<Booking | null>(null);
-  const [confirmCancel, setConfirmCancel] = useState(false);
   const [showDetails, setShowDetails] = useState(false);
 
   const formattedFrom = format(
@@ -38,139 +17,49 @@ export default function OverviewItem({ booking }: { booking: Booking }) {
     "hh:mm aa"
   );
   return (
-    <div className="border border-black/10 rounded-lg py-2 px-4 flex flex-wrap lg:items-center gap-3 justify-between">
-      <div className="space-y-1 self-start">
-        <p>
-          <span className="font-medium">
-            {booking.guest?.fullName ||
-              booking.user?.personalInformation.fullName}
-          </span>
-          <span
-            className={cn(
-              `text-sm py-1 px-4 ml-3 rounded-lg`,
-              booking.status === "INPROGRESS" && "bg-info/20 text-info-text",
-              booking.status === "COMPLETED" && "bg-success/20 text-success",
-              (booking.status === "PENDING" || booking.status === "UNPAID") &&
-                "bg-warning/20 text-warning-text",
-              booking.status === "CANCELLED" && "bg-error/20 text-error"
-            )}
-          >
-            {BookingStatusLabels[booking.status]}
-          </span>
-        </p>
-        <p className="text-xs text-secondary-700/70 capitalize">
-          {booking.serviceType.name}
-          <span className="h-2 w-2 bg-surface-300 inline-block rounded-full mx-2" />
-          {formattedFrom} - {formattedTo}
-        </p>
-        <p className="text-xs text-secondary-700/70">
-          Cleaner:
-          <span className="ml-2">
-            {booking.cleaners.map((i) => i.fullName).join(", ") || "N/A"}
-          </span>
-        </p>
-      </div>
-      <div className="flex items-end ml-auto justify-end gap-3">
-        {booking.user?.role === "ADMIN" && (
-          <Button
-            size="2xs"
-            variant={"secondary-outline"}
-            className="text-xs flex items-center justify-center gap-1 border-black/10"
-            onClick={() => setEditBooking(!editBooking)}
-          >
-            <EditIcon className="size-4" />
-            Edit
-          </Button>
-        )}
-        <Button
-          size="2xs"
-          variant={"secondary-outline"}
-          className="text-xs flex items-center justify-center gap-1 border-black/10"
-        >
-          <CallIcon className="size-4" />
-          Call
-        </Button>
-        {booking.status === "PENDING" && booking.cleaners.length > 0 && (
-          <Button
-            size="2xs"
-            variant={"secondary-outline"}
-            className="text-xs flex items-center justify-center gap-1 border-black/10 bg-success/15"
-            onClick={() => mutateStart({ bookingId: booking.id })}
-            disabled={isStarting}
-          >
-            {isStarting ? "Starting" : "Start"}
-          </Button>
-        )}
-        {booking.status === "INPROGRESS" && (
-          <Button
-            size="2xs"
-            variant={"secondary-outline"}
-            className="text-xs flex items-center justify-center gap-1 border-black/10 bg-success/15"
-            onClick={() => mutateComplete({ bookingId: booking.id })}
-            disabled={isCompleting}
-          >
-            {isCompleting ? "Completing" : "Complete"}
-          </Button>
-        )}
-        {booking.status !== "CANCELLED" && booking.status !== "COMPLETED" && (
-          <Button
-            disabled={isCancelling}
-            size="2xs"
-            variant={"destructive"}
-            className="text-xs flex items-center justify-center gap-1 border-black/10"
-            onClick={() => setConfirmCancel(true)}
-          >
-            <TrashIcon className="size-4" />
-            {isCancelling ? "Cancelling" : "Cancel"}
-          </Button>
-        )}
-        {!booking.cleaners.length &&
-          (booking.status === "PENDING" || booking.status === "UNPAID") && (
-            <Button
-              size={"2xs"}
-              variant={"secondary"}
-              onClick={() => setAssignCleaners(booking)}
+    <>
+      <div className="border border-black/10 rounded-lg py-2 px-4 flex items-start gap-3 justify-between">
+        <div className="space-y-1 self-start">
+          <p>
+            <span className="font-medium">
+              {booking.guest?.fullName ||
+                booking.user?.personalInformation.fullName}
+            </span>
+            <span
+              className={cn(
+                `text-sm py-1 px-4 ml-3 rounded-lg`,
+                booking.status === "INPROGRESS" && "bg-info/20 text-info-text",
+                booking.status === "COMPLETED" && "bg-success/20 text-success",
+                (booking.status === "PENDING" || booking.status === "UNPAID") &&
+                  "bg-warning/20 text-warning-text",
+                booking.status === "CANCELLED" && "bg-error/20 text-error"
+              )}
             >
-              Assign Cleaner
-            </Button>
-          )}
-        <Button
-          size={"2xs"}
-          variant={"secondary"}
-          onClick={() => setShowDetails(true)}
-        >
-          Details
-        </Button>
+              {BookingStatusLabels[booking.status]}
+            </span>
+          </p>
+          <p className="text-xs text-secondary-700/70 capitalize">
+            {booking.serviceType.name}
+            <span className="h-2 w-2 bg-surface-300 inline-block rounded-full mx-2" />
+            {formattedFrom} - {formattedTo}
+          </p>
+          <p className="text-xs text-secondary-700/70">
+            Cleaner:
+            <span className="ml-2">
+              {booking.cleaners.map((i) => i.fullName).join(", ") || "N/A"}
+            </span>
+          </p>
+        </div>
+        <div className="flex items-end ml-auto justify-end">
+          <BookingActions booking={booking} />
+        </div>
       </div>
-      {editBooking && (
-        <EditBooking booking={booking} onClose={() => setEditBooking(false)} />
-      )}
-      {assignCleaners && (
-        <AssignCleaner
-          booking={booking}
-          onClose={() => setAssignCleaners(null)}
-        />
-      )}
-      {confirmCancel && (
-        <ConfirmModal
-          accent="destructive"
-          title="Cancel Booking"
-          description={`Are you sure you want to cancel booking #${booking.id.toString().padStart(3, "0")}? This action cannot be undone.`}
-          onCancel={() => setConfirmCancel(false)}
-          onConfirm={async () => {
-            await mutateCancel({ bookingId: booking.id });
-            setConfirmCancel(false);
-          }}
-          loading={isCancelling}
-          open={confirmCancel}
-        />
-      )}
       {showDetails && (
         <BookingDetails
           bookingId={booking.id}
           onClose={() => setShowDetails(false)}
         />
       )}
-    </div>
+    </>
   );
 }
--- src/app/admin/(authed)/bookings/_components/ListItem.tsx
+++ src/app/admin/(authed)/bookings/_components/ListItem.tsx
@@ -1,23 +1,8 @@
 "use client";
 import { Booking } from "@/app/admin/types";
-import CallIcon from "@/components/icons/CallIcon";
-// import EditIcon from "@/components/icons/EditIcon";
-import TrashIcon from "@/components/icons/TrashIcon";
-import { Button } from "@/components/ui/Button";
 import { cn } from "@/lib/utils";
 import { format } from "date-fns";
-import React, { useState } from "react";
-import {
-  useCancelBookingMutation,
-  useCompleteBookingMutation,
-  useStartBookingMutation
-} from "../../_services/mutations";
-import EditIcon from "@/components/icons/EditIcon";
-import EditBooking from "./EditBooking";
-import AssignCleaner from "./AssignCleaner";
-import ConfirmModal from "@/components/common/ConfirmModal";
-import BookingDetails from "./BookingDetails";
-import Checkbox from "@/components/ui/Checkbox";
+import BookingActions from "../../_components/BookingActions";
 
 interface StatusType {
   label: string;
@@ -31,19 +16,6 @@ export default function ListItem({
   status: StatusType;
   booking: Booking;
 }) {
-  const { mutateAsync: mutateCancel, isPending: isCancelling } =
-    useCancelBookingMutation();
-  const { mutateAsync: mutateStart, isPending: isStarting } =
-    useStartBookingMutation();
-  const { mutateAsync: mutateComplete, isPending: isCompleting } =
-    useCompleteBookingMutation();
-
-  const [editBooking, setEditBooking] = useState(false);
-  const [assignCleaners, setAssignCleaners] = useState<Booking | null>(null);
-  const [confirmCancel, setConfirmCancel] = useState(false);
-  const [unsubscribe, setUnsubscribe] = useState(false);
-  const [showDetails, setShowDetails] = useState(false);
-
   const bookingName =
     booking.guest?.fullName ||
     booking.user?.personalInformation?.fullName ||
@@ -70,194 +42,76 @@ export default function ListItem({
     .join(", ");
 
   return (
-    <>
-      <div className="p-4 border border-black/10 rounded-lg flex flex-col lg:flex-row justify-between gap-4">
-        <div>
-          <div className="flex items-center gap-3">
-            <span className="font-medium">{bookingName || "Guest"}</span>
-            <div
-              className={cn(
-                "py-1.5 px-3 text-sm rounded-md inline-flex items-center",
-                status.value === "PENDING" || booking.status === "UNPAID"
-                  ? "bg-warning/20 text-warning-text"
-                  : status.value === "INPROGRESS"
-                    ? "bg-info/20 text-info-text"
-                    : status.value === "COMPLETED"
-                      ? "bg-success/10 text-success"
-                      : status.value === "CANCELLED"
-                        ? "bg-error/10 text-error"
-                        : ""
-              )}
-            >
-              {status.label}
-            </div>
-            <span className="text-xs text-secondary-700/70">
-              #{bookingNumber.toString().padStart(3, "0")}
-            </span>
+    <div className="p-4 border border-black/10 rounded-lg flex flex-col lg:flex-row justify-between gap-4">
+      <div>
+        <div className="flex items-center gap-3">
+          <span className="font-medium">{bookingName || "Guest"}</span>
+          <div
+            className={cn(
+              "py-1.5 px-3 text-sm rounded-md inline-flex items-center",
+              status.value === "PENDING" || booking.status === "UNPAID"
+                ? "bg-warning/20 text-warning-text"
+                : status.value === "INPROGRESS"
+                  ? "bg-info/20 text-info-text"
+                  : status.value === "COMPLETED"
+                    ? "bg-success/10 text-success"
+                    : status.value === "CANCELLED"
+                      ? "bg-error/10 text-error"
+                      : ""
+            )}
+          >
+            {status.label}
           </div>
-
-          <div className="mt-4 lg:mt-6 text-xs flex flex-wrap gap-x-8 gap-y-4">
-            <div className="space-y-1">
-              <p>
-                <span className="font-medium mr-2">Service:</span>
-                <span className="capitalize">{service}</span>
-              </p>
-              <p>
-                <span className="font-medium mr-2">Date & Time:</span>
-                <span>{dateTime}</span>
-              </p>
-              <p>
-                <span className="font-medium mr-2">Cleaners:</span>
-                {cleaners?.length ? (
-                  cleaners.map((i) => i.fullName).join(", ")
-                ) : (
-                  <span className="text-error">Unassigned</span>
-                )}
-              </p>
-              {notes && (
-                <p className="mt-2 text-xs text-secondary-700/70">
-                  <span className="font-medium mr-2">Notes:</span>
-                  <span>{notes}</span>
-                </p>
-              )}
-            </div>
-            <div className="space-y-1">
-              <p>
-                <span className="font-medium mr-2">Address:</span>
-                <span>{address}</span>
-              </p>
-              <p>
-                <span className="font-medium mr-2">Phone:</span>
-                <span>{phone}</span>
-              </p>
-              <p>
-                <span className="font-medium mr-2">Price:</span>
-                <span>${price}</span>
-              </p>
-            </div>
-          </div>
+          <span className="text-xs text-secondary-700/70">
+            #{bookingNumber.toString().padStart(3, "0")}
+          </span>
         </div>
 
-        <div className="w-full lg:w-auto">
-          <div className="flex items-start justify-end gap-3 *:flex-1 lg:*:flex-0 flex-wrap">
-            {booking.user?.role === "ADMIN" && (
-              <Button
-                size="2xs"
-                variant={"secondary-outline"}
-                className="text-xs flex items-center justify-center gap-1 border-black/10"
-                onClick={() => setEditBooking(!editBooking)}
-              >
-                <EditIcon className="size-4" />
-                Edit
-              </Button>
-            )}
-            <Button
-              size="2xs"
-              variant={"secondary-outline"}
-              className="text-xs flex items-center justify-center gap-1 border-black/10"
-            >
-              <CallIcon className="size-4" />
-              Call
-            </Button>
-            {status.value === "PENDING" && cleaners.length > 0 && (
-              <Button
-                size="2xs"
-                variant={"secondary-outline"}
-                className="text-xs flex items-center justify-center gap-1 border-black/10 bg-success/15"
-                onClick={() => mutateStart({ bookingId: booking.id })}
-                disabled={isStarting}
-              >
-                {isStarting ? "Starting" : "Start"}
-              </Button>
-            )}
-            {status.value === "INPROGRESS" && (
-              <Button
-                size="2xs"
-                variant={"secondary-outline"}
-                className="text-xs flex items-center justify-center gap-1 border-black/10 bg-success/15"
-                onClick={() => mutateComplete({ bookingId: booking.id })}
-                disabled={isCompleting}
-              >
-                {isCompleting ? "Completing" : "Complete"}
-              </Button>
-            )}
-            {status.value !== "CANCELLED" && status.value !== "COMPLETED" && (
-              <Button
-                disabled={isCancelling}
-                size="2xs"
-                variant={"destructive"}
-                className="text-xs flex items-center justify-center gap-1 border-black/10"
-                onClick={() => setConfirmCancel(true)}
-              >
-                <TrashIcon className="size-4" />
-                {isCancelling ? "Cancelling" : "Cancel"}
-              </Button>
+        <div className="mt-4 lg:mt-6 text-xs flex flex-wrap gap-x-8 gap-y-4">
+          <div className="space-y-1">
+            <p>
+              <span className="font-medium mr-2">Service:</span>
+              <span className="capitalize">{service}</span>
+            </p>
+            <p>
+              <span className="font-medium mr-2">Date & Time:</span>
+              <span>{dateTime}</span>
+            </p>
+            <p>
+              <span className="font-medium mr-2">Cleaners:</span>
+              {cleaners?.length ? (
+                cleaners.map((i) => i.fullName).join(", ")
+              ) : (
+                <span className="text-error">Unassigned</span>
+              )}
+            </p>
+            {notes && (
+              <p className="mt-2 text-xs text-secondary-700/70">
+                <span className="font-medium mr-2">Notes:</span>
+                <span>{notes}</span>
+              </p>
             )}
           </div>
-          <div className="mt-4 lg:mt-6 flex justify-end *:flex-1 lg:*:flex-0 gap-3">
-            {!cleaners.length &&
-              (status.value === "PENDING" || booking.status === "UNPAID") && (
-                <Button
-                  size={"2xs"}
-                  variant={"secondary"}
-                  onClick={() => setAssignCleaners(booking)}
-                >
-                  Assign Cleaner
-                </Button>
-              )}
-            <Button
-              size={"2xs"}
-              variant={"secondary"}
-              onClick={() => setShowDetails(true)}
-            >
-              Details
-            </Button>
+          <div className="space-y-1">
+            <p>
+              <span className="font-medium mr-2">Address:</span>
+              <span>{address}</span>
+            </p>
+            <p>
+              <span className="font-medium mr-2">Phone:</span>
+              <span>{phone}</span>
+            </p>
+            <p>
+              <span className="font-medium mr-2">Price:</span>
+              <span>${price}</span>
+            </p>
           </div>
         </div>
       </div>
-      {editBooking && (
-        <EditBooking booking={booking} onClose={() => setEditBooking(false)} />
-      )}
-      {assignCleaners && (
-        <AssignCleaner
-          booking={booking}
-          onClose={() => setAssignCleaners(null)}
-        />
-      )}
-      {confirmCancel && (
-        <ConfirmModal
-          accent="destructive"
-          title="Cancel Booking"
-          description={`Are you sure you want to cancel booking #${bookingNumber.toString().padStart(3, "0")}? This action cannot be undone.`}
-          onCancel={() => setConfirmCancel(false)}
-          onConfirm={async () => {
-            await mutateCancel({
-              bookingId: booking.id,
-              unsubscribe: unsubscribe
-            });
-            setConfirmCancel(false);
-          }}
-          loading={isCancelling}
-          open={confirmCancel}
-        >
-          <div className="mb-6 flex items-center gap-2">
-            <Checkbox
-              id="unsubscribe"
-              checked={unsubscribe}
-              onChange={(e) => setUnsubscribe(e.target.checked)}
-              disabled={isCancelling}
-              label="Unsubscribe from cleaning service"
-              accent="secondary"
-            />
-          </div>
-        </ConfirmModal>
-      )}
-      {showDetails && (
-        <BookingDetails
-          bookingId={booking.id}
-          onClose={() => setShowDetails(false)}
-        />
-      )}
-    </>
+
+      <div className="flex items-start justify-end">
+        <BookingActions booking={booking} />
+      </div>
+    </div>
   );
 }
